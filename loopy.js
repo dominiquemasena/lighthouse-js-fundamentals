a44323e8aeb17d0ce142197eeaf4eae42bb4eecc
Dominique Masena <dominique.masena@live.ca>
2020-06-07 22:41:01 +0000

    loopy.js

---
+++ loopy.js
@@ -0,0 +1,13 @@
+for (i = 100; i <= 200; i++){ 
+  if (0 === i % 3 && 0 === i % 4) {
+    console.log("LoopyLighthouse");} 
+    else if
+  (0 === i % 3 ){console.log("Loopy")} 
+  else if (0 === i % 4) {
+    console.log("Lighthouse");
+  }  
+    else {
+      console.log(i);
+  }
+}
+
