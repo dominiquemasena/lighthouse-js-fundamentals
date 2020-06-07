1db8aa5f03d53d56e2a38118da5cc8160b477431
Dominique Masena <dominique.masena@live.ca>
2020-06-07 06:26:55 +0000

    commit all

---
+++ whichSchool.js
@@ -0,0 +1,8 @@
+let age = 5;
+
+if (age < 13) {
+  console.log("You\'re going to Elementary School!");}
+   else if (age >= 18 && age <= 18) {
+  console.log("You\'re going to Elementary School!")}
+else {
+  console.log("You\'re going to Elementary School!")}
