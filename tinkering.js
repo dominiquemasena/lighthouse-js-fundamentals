45e8a4404a1e9da6b058743ace682137e303f341
Dominique Masena <dominique.masena@live.ca>
2020-06-10 17:37:23 +0000

    tinkering.js

--- tinkering.js
+++ tinkering.js
@@ -1,5 +1,7 @@
-var my_string = "green";
-
-for (var i = 0; i < my_string.length; i++) {
-  console.log(my_string.charCodeAt(my_string));
+function multiply(a, b) {
+  return a * b;
 }
+
+const result = multiply(2, 4);
+
+console.log(result);
